d9713b1753b6e597c8847ac17c8c382974209d39
Toni Schranz <toni.schranz@gmail.com>
2019-06-09 20:52:58 +0200

    asd

--- plugins/_code/controller/default.controller.php
+++ plugins/_code/controller/default.controller.php
@@ -4,10 +4,11 @@ use tsd\serve\Controller;
 
 class DefaultController extends Controller
 {
-
+  /**
+    @SecurityGroup developer
+   */
   function showIndex ()
-  {
-    
+  {    
     $this->render ('index', []);
   }
 
