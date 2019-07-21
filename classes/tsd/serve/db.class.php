83eb0c328c3629176b6e072ee1c1f975996cd632
Toni Schranz <toni.schranz@gmail.com>
2019-07-21 19:57:00 +0200

    reorg

--- classes/tsd/serve/db.class.php
+++ classes/tsd/serve/db.class.php
@@ -2,10 +2,25 @@
 
 namespace tsd\serve;
 
-class DB
+interface DB
 {
-    function __construct(array $config)
+    function select();
+}
+
+
+/**
+ * @Default
+ * @Mode mysql
+ */
+class MySQLDB implements DB
+{
+    function __construct(string $host, string $username, string $password, string $database)
     {
         
+    }
+
+    function select()
+    {
+
     }
 }
