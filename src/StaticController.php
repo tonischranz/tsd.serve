daa90601e87c988af6f1ec64771e81253a0405b4
Toni Schranz <toni.schranz@gmail.com>
2022-03-16 09:47:40 +0100

    asdf

--- src/StaticController.php
+++ src/StaticController.php
@@ -74,7 +74,8 @@ class StaticController extends Controller
             div {margin-top: .5em;}
             body {padding-bottom: 3.5em }
             body>header { background-color:var(--header-bg-color, #111); }
-            body>header>nav ul {list-style-type:none; padding-inline-start:0; margin-block-start:0; margin-block-end:0; font-size:4rem;}    
+            body>header>nav ul {list-style-type:none; padding-inline-start:0; margin-block-start:0; margin-block-end:0; font-size:4rem;}
+            body>header>nav ul ul {padding-inline-start:1em; font-size:.7em;}
             body>main { min-height:calc(100vh - 12rem);}
             body>main>*, body>footer>* { overflow-x:auto; scrollbar-color: var(--scrollbar-color, #000) var(--scrollbar-bg-color, #222); scrollbar-width: thin; }
             body>main *::-webkit-scrollbar, body>footer *::-webkit-scrollbar { width : .5em; height: .5em; }
