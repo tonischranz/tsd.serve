713e5b7ce190faa023d429d1ef77ff07e4091c57
Toni Schranz <toni.schranz@gmail.com>
2022-03-17 11:48:33 +0100

    bs

--- src/StaticController.php
+++ src/StaticController.php
@@ -111,7 +111,7 @@ class StaticController extends Controller
             details.member summary::before {content:"🧒";}
             details[open] summary::before {content:"❌";}
             details[open] {background-color:#abc}
-            :focus-visible {outline:none;}
+            :focus-visible {outline:none; box-shadow:none;}
             EOCSS;
             return new TextResult($css, 'text/css');
         }
