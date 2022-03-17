5d82fbc68a03045950339e414e80bce4808752b4
Toni Schranz <toni.schranz@gmail.com>
2022-03-17 11:23:37 +0100

    focus outline

--- src/StaticController.php
+++ src/StaticController.php
@@ -111,6 +111,7 @@ class StaticController extends Controller
             details.member summary::before {content:"🧒";}
             details[open] summary::before {content:"❌";}
             details[open] {background-color:#abc}
+            :focus-visible {outline:none;}
             EOCSS;
             return new TextResult($css, 'text/css');
         }
