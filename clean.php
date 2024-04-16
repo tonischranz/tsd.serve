dc95700d19d2648a4c244f98dc3743e86dd91b1b
Toni Schranz <toni.schranz@gmail.com>
2024-04-16 18:06:15 +0200

    Update clean.php

--- clean.php
+++ clean.php
@@ -701,7 +701,7 @@ if ($no_cfg) {
 
     <div id="content">
         <?php $eu = (@$_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']; ?>
-	<?php if ($_SERVER['HTTP_HOST'] != 'localhost:8000': ?>
+	<?php if ($_SERVER['HTTP_HOST'] != 'localhost:8000') : ?>
           <div class="gap"></div>
 	  <div class="c">            
 	  <img src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=<?=urlencode($eu)?>" alt="QR-Code"?>
