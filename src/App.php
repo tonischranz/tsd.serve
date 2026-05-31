00fca575a70d3a723cd38f2e0678c6160b6f33a3
Toni Schranz <toni.schranz@gmail.com>
2026-05-31 18:01:39 +0200

    avoid warning

--- src/App.php
+++ src/App.php
@@ -114,7 +114,7 @@ class App
                     '_GET' => $_GET, '_COOKIE' => $_COOKIE,
                     '_POST' => $_POST, '_FILES' => $_FILES
                 ],
-                $_SERVER['HTTP_ACCEPT']
+                isset($_SERVER['HTTP_ACCEPT']) ? $_SERVER['HTTP_ACCEPT'] : ''
             );            
         } catch (\Exception $e) {
             ob_end_clean();
